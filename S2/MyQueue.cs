ba0c3694c79f96903ebd832642c90d38210f3549
ahmed <ahmed@AHMED_SAMY>
2025-08-27 23:37:47 +0300

    Q11

---
+++ S2/MyQueue.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S2
+{
+    internal class MyQueue 
+    {
+
+        #region Attributes
+
+        private int _count; 
+        const int defaultCapacity = 4;
+        private object[] items;
+
+        #endregion
+
+        #region Proerty
+
+        public int Count
+        {
+            get { return _count; }
+        } 
+
+        public int Capacity { get; set; }
+
+        #endregion
+
+        
+        #region Constructor
+        public MyQueue()
+        {
+            items = new object[0];
+            Capacity = 0;
+            _count = 0;
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void MyEnqueue(object item)
+        {
+            if(items is not null)
+            {
+                if (_count == 0)
+                {
+                    items = new object[defaultCapacity];
+                    Capacity = defaultCapacity;
+                    items[0] = item;
+                    _count++;
+                }
+                else if (_count == Capacity)
+                {
+                      
+                    object[] newItems = new object[Capacity * 2];
+                    
+                    
+                    for (int i = 0; i < _count; i++)
+                    {
+                        newItems[i] = items[i];
+                    }
+                    
+                    items = newItems;
+                    
+                    Capacity *= 2;
+                      
+                    items[_count] = item;
+                    _count++;
+                }
+                else
+                {
+                    items[_count] = item;
+                    _count++;
+                }
+
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Traverse()
+        {
+            if(items is not null && _count > 0)
+                for(int i = 0; i < _count; i++)
+                    Console.WriteLine(items[i]);
+
+        }
+
+        #endregion
+    }
+}
--- S2/Program.cs
+++ S2/Program.cs
@@ -6,7 +6,7 @@ namespace S2
     internal class Program
     {
         
-        public static int NumberOfGreaterThanTheNumber(int number , int[] arr)
+        public static int NumberOfTimesGreaterThanTheNumber(int number , int[] arr)
         {
             int count = 0;
             if (arr is not null && arr.Length > 0)
@@ -27,17 +27,16 @@ namespace S2
         {
             if (arr is not null && arr.Length > 0)
             {
-                int left = 0;
-                int right = arr.Length - 1;
-                while (left < right)
+
+                int LastIndex = arr.Length - 1;
+                for(int i = 0;i < arr.Length; i++)
                 {
-                    if (arr[left] != arr[right])
-                    {
+                    
+                    if (arr[i] != arr[LastIndex])
                         return false;
-                    }
-                    left++;
-                    right--;
+                    LastIndex--;
                 }
+
                 return true;
             }
             return false;
@@ -110,6 +109,75 @@ namespace S2
             }
         }
 
+        public static void RemoveAllOddNumbers(ref ArrayList arrayList)
+        {
+            if (arrayList is not null && arrayList.Count > 0)
+            {
+                for (int i = 0; i < arrayList.Count; i++)
+                {
+                    int? num = (int?)arrayList[i];
+                    
+                    if (num % 2 != 0)
+                    {
+                        arrayList.Remove(num);
+                        i--;
+                    }
+                }
+            }
+        }
+
+        public static void PushInTheStack(ref Stack<int> stack, params int[] numbers)
+        {
+            for(int i = 0; i < numbers.Length; i++)
+                stack.Push(i);
+              
+        }
+
+        public static void FintTargetInTheStack(Stack<int> stack, int target)
+        {
+            int Count = 0;
+            bool Flag = false;
+            foreach (int i in stack)
+            {
+                Count++;
+                if (i == target)
+                {
+                    Flag = true;
+                    break;
+                }
+            }
+            if (Flag)
+                Console.WriteLine($"Target was found successfully and the count = {Count}");
+            else
+                Console.WriteLine("Target was not found");
+
+        }
+
+        public static Queue<int> ReverseFirstKElements(Queue<int> q, int k)
+        {
+            if (q == null || k > q.Count || k <= 0)
+                return q;
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < k; i++)
+            {
+                stack.Push(q.Dequeue());
+            }
+
+            while (stack.Count > 0)
+            {
+                q.Enqueue(stack.Pop());
+            }
+
+            int size = q.Count;
+            for (int i = 0; i < size - k; i++)
+            {
+                q.Enqueue(q.Dequeue());
+            }
+
+            return q;
+        }
         static void Main(string[] args)
         {
             #region Q1
@@ -145,7 +213,7 @@ namespace S2
             //{
             //    Console.WriteLine("please Enter The Number to compare");
             //    int.TryParse(Console.ReadLine(), out int number);
-            //    arr02[i] = NumberOfGreaterThanTheNumber(number, arr);
+            //    arr02[i] = NumberOfTimesGreaterThanTheNumber(number, arr);
             //    Console.WriteLine();
             //}
 
@@ -225,15 +293,196 @@ namespace S2
             //    Console.WriteLine(i);
             //}
 
+            #endregion
+
+            #region Q6
+
+            //ArrayList arrayList = new ArrayList() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //RemoveAllOddNumbers(ref arrayList);
+            //foreach (var item in arrayList)
+            //    Console.WriteLine(item);
+
+
+            #endregion
+
+            #region Q7
+
+            //MyQueue myQueue = new MyQueue();
+            //Console.WriteLine($"{myQueue.Capacity} , {myQueue.Count}"); // 0 , 0
+            //myQueue.MyEnqueue(1);
+            //myQueue.MyEnqueue("Apple");
+            //myQueue.MyEnqueue(5.28);
+            //myQueue.MyEnqueue(5.28);
+            //myQueue.MyEnqueue(5.28);
+            //Console.WriteLine($"{myQueue.Capacity} , {myQueue.Count}"); // 8 , 5
+
+            //myQueue.Traverse();
 
 
 
             #endregion
 
+            #region Q8
+
+            //Stack<int> stack = new Stack<int>();
+            //PushInTheStack(ref stack, 1, 2, 3, 4, 5, 6);
+
+            //bool isParse;
+            //int Num;
+
+            //do
+            //{
+            //    Console.WriteLine("please Enter The Target Number :");
+            //    isParse = int.TryParse(Console.ReadLine(), out Num);
+            //    if(!isParse)
+            //        Console.WriteLine("Invalid Input");
+
+            //} while(!isParse);
+
+            //FintTargetInTheStack(stack,Num);
+
+            #endregion
+
+            #region Q9
+
+            //Console.WriteLine("please Enter The Size of two Arrays");
+            //string[] sizesInput = Console.ReadLine().Split(',');
+            //int n = int.Parse(sizesInput[0].Trim());
+            //int m = int.Parse(sizesInput[1].Trim());
 
 
+            //Console.WriteLine("please Enter the first array");
+            //string arr1Input = Console.ReadLine().Trim('[', ']');
+            //string[] arr1Parts = arr1Input.Split(',');
+            //int[] arr1 = new int[n];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    arr1[i] = int.Parse(arr1Parts[i].Trim());
+            //}
+
+            //Console.WriteLine("please Enter the second array");
+            //string arr2Input = Console.ReadLine().Trim('[', ']');
+            //string[] arr2Parts = arr2Input.Split(',');
+            //int[] arr2 = new int[m];
+            //for (int i = 0; i < m; i++)
+            //{
+            //    arr2[i] = int.Parse(arr2Parts[i].Trim());
+            //}
+
+            //List<int> result = new List<int>();
 
 
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < m; j++)
+            //    {
+            //        if (arr1[i] == arr2[j])
+            //        {
+            //            result.Add(arr1[i]);
+            //            arr2[j] = int.MinValue;
+            //            break;
+            //        }
+            //    }
+            //}
+
+            //Console.WriteLine("the intersection");
+            //Console.Write("[");
+            //for (int i = 0; i < result.Count; i++)
+            //{
+            //    Console.Write(result[i]);
+            //    if (i < result.Count - 1) Console.Write(",");
+            //}
+            //Console.WriteLine("]");
+
+
+            #endregion
+
+            #region Q10
+
+            //Console.WriteLine("Please enter the array");
+            //string inputArray = Console.ReadLine().Trim('[', ']');
+            //string[] parts = inputArray.Split(',');
+            //int n = parts.Length;
+            //int[] arr = new int[n];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    arr[i] = int.Parse(parts[i].Trim());
+            //}
+
+            //Console.WriteLine("Please enter the target sum:");
+            //int target = int.Parse(Console.ReadLine());
+
+            //int start = 0, currSum = 0;
+            //bool found = false;
+            //List<int> result = new List<int>();
+
+            //for (int end = 0; end < n; end++)
+            //{
+            //    currSum += arr[end];
+
+            //    while (currSum > target && start <= end)
+            //    {
+            //        currSum -= arr[start];
+            //        start++;
+            //    }
+
+            //    if (currSum == target)
+            //    {
+            //        for (int k = start; k <= end; k++)
+            //        {
+            //            result.Add(arr[k]);
+            //        }
+            //        found = true;
+            //        break;
+            //    }
+            //}
+
+            //if (found)
+            //{
+            //    Console.Write("[");
+            //    for (int i = 0; i < result.Count; i++)
+            //    {
+            //        Console.Write(result[i]);
+            //        if (i < result.Count - 1) Console.Write(", ");
+            //    }
+            //    Console.WriteLine("]");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No subarray found with the given sum.");
+            //}
+
+            #endregion
+
+            #region Q11
+
+            //Console.WriteLine("Enter the queue");
+            //string input = Console.ReadLine().Trim('[', ']');
+            //string[] parts = input.Split(',');
+            //Queue<int> q = new Queue<int>();
+            //foreach (string s in parts)
+            //{
+            //    q.Enqueue(int.Parse(s.Trim()));
+            //}
+
+            //Console.WriteLine("Enter K");
+            //int k = int.Parse(Console.ReadLine());
+
+            //q = ReverseFirstKElements(q, k);
+
+            //Console.Write("[");
+            //int count = q.Count;
+            //for (int i = 0; i < count; i++)
+            //{
+            //    int val = q.Dequeue();
+            //    Console.Write(val);
+            //    if (i < count - 1) Console.Write(", ");
+            //    q.Enqueue(val);
+            //}
+            //Console.WriteLine("]");
+
+
+            #endregion
 
         }
     }
